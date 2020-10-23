c69177d51f2ef9d01ae44811a5e0e01bbce7e1a2
prlouis-laptop <pierrereama.louis@gmail.com>
2020-10-23 13:16:39 -0400

    Light Dark Mode project Practice changing light to dark mode style
    working on javascript functionality
    
     Changes to be committed:
    	new file:   Thumbs.db
    	new file:   favicon.png
    	new file:   img/undraw_conceptual_idea_dark.svg
    	new file:   img/undraw_conceptual_idea_light.svg
    	new file:   img/undraw_feeling_proud_dark.svg
    	new file:   img/undraw_feeling_proud_light.svg
    	new file:   img/undraw_proud_coder_dark.svg
    	new file:   img/undraw_proud_coder_light.svg
    	new file:   index.html
    	new file:   script.js
    	new file:   style.css

---
+++ light-dark-mode/script.js
@@ -0,0 +1,12 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+
+// Switch Theme Dynamically
+function switchTheme(event) {
+    if(event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
+}
+// Event Listner
+toggleSwitch.addEventListener('change', switchTheme);
