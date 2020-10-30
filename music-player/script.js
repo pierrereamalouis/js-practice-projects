44d8682fac5c53ccec7f2919308b55a3a0d2ab9e
prlouis <pierrereama.louis@gmail.com>
2020-10-30 11:51:01 -0400

    project completed

--- music-player/script.js
+++ music-player/script.js
@@ -113,11 +113,15 @@ function updateProgressBar(e) {
 
 // Set Progress Bar
 function setProgressBar(e) {
-    
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const {duration} = music;
+    music.currentTime = (clickX / width) * duration; 
 }
 
 // Event Listeners
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
 music.addEventListener('timeupdate', updateProgressBar);
+music.addEventListener('ended', nextSong);
 progressContainer.addEventListener('click', setProgressBar);
